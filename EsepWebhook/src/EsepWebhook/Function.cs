3373a04c0ed02af70a08d20923a8df8131bcd859
Amanda Yu <92806914+yuamanda@users.noreply.github.com>
2023-11-10 21:45:17 -0500

    Update Function.cs

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -16,7 +16,7 @@ public class Function
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public string FunctionHandler(object input, ILambdaContext context)
     {
 	dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
         
